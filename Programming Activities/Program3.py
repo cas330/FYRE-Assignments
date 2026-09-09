d0a5169332d57bc86b73bd8f0d2b86c47e058394
Cal Stewart <cas330@lehigh.edu>
2026-09-09 15:19:31 -0400

    Add files via upload

---
+++ Programming Activities/Program3.py
@@ -0,0 +1,7 @@
+# This program was created in Arduino Lab for MicroPython
+# This program is used to demonstrate how variables work
+
+
+mclovin = "Cal P Stewart"
+
+print(mclovin) #output the contents of the variable to REPL
